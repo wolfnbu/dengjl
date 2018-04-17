47f979d7310ce03924f741be73fd35fefbf405ae
allandeng_mac <allandeng@mac.com>
2018-04-17 22:30:05 +0800

    init my welcome page

---
+++ welcome.py
@@ -0,0 +1,14 @@
+# This is welcome page of my server - dengjl.com
+
+
+print "Content-type:text/html"
+print
+print "<html>"
+print "<head>"
+print "<meta charset=\"utf-8\">"
+print "<title>CGI</title>"
+print "</head>"
+print "<body>"
+print "<h2>Welcome</h2>"
+print "</body>"
+print "</html>"
